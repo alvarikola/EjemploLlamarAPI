3b49189c1fcb2ebe21d3dbd018c8638f27a31e0c
alvarikola <alvaroe484@gmail.com>
2025-01-24 09:10:40 +0000

    terminado

--- app/src/main/java/com/example/ejemplollamarapi/network/product/ProductService.kt
+++ app/src/main/java/com/example/ejemplollamarapi/network/product/ProductService.kt
@@ -2,6 +2,7 @@ package com.example.ejemplollamarapi.network.product
 
 import com.example.ejemplollamarapi.network.RetrofitHelper
 import com.example.ejemplollamarapi.network.product.model.ProductListResponse
+import com.example.ejemplollamarapi.network.product.model.ProductResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -11,6 +12,13 @@ class ProductService {
     suspend fun getAllProducts(): ProductListResponse {
         return withContext(Dispatchers.IO) {
             val response = retrofit.create(ProductClient::class.java).getAllProducts()
+            return@withContext response.body()!!
+        }
+    }
+
+    suspend fun searchProduct(productoBuscar: String): ProductListResponse {
+        return withContext(Dispatchers.IO) {
+            val response = retrofit.create(ProductClient::class.java).searchProduct(productoBuscar)
             return@withContext response.body()!!
         }
     }
